dedd396aaa48870a1dd8aaef778df380c25ce2e8
Amneet Singh <+>
2023-01-04 11:04:28 -0500

    news

--- src/News.jsx
+++ src/News.jsx
@@ -1,10 +1,35 @@
 import { useNavigate } from "react-router-dom"
+import { useEffect, useState } from 'react'
 
 export default function News() {
     const navigate = useNavigate();
-    return (
-        <div>
+    const [ articles, setArticles ] = useState([])
+    useEffect(()=> {
+        const request = async () => {
+            let req = await fetch("http://localhost:3000/articles")
+            let res = await req.json()
+            setArticles(res)
+        }
+        request()
+    }, [])
 
+    return (
+        <div className="article">
+            {
+                articles.map((article) => {
+                    return (
+                        <div className="articleCard">
+                            <h2>{article.title}</h2>
+                            <img src={article.image} className="articleImg"/>
+                            <p>Written By {article.author}</p>
+                            <p>{article.published}</p>
+                            <p>{article.content}</p>
+
+                        </div>
+                    )
+                })
+            }
+            
         </div>
     )
 }
