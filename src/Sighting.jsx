27fc8cc7568111e152e4ddd235d7773210b5f4cb
Amneet Singh <+>
2023-01-06 16:14:44 -0500

    final final

--- src/Sighting.jsx
+++ src/Sighting.jsx
@@ -1,47 +1,16 @@
 import SightingCard from './SightingCard'
 import { useState, useEffect } from 'react'
-
-export default function Sighting({ sightings, setSightings }) {
-
-    const [sort, setSort] = useState(false)
-
-    const sortedSightings = sightings.sort((a, b) => a.likecount > b.likecount ? -1 : 1)
-
-    const handleSort = () => {
-        setSort(true)
-        console.log(sort)
-    }
-
-    useEffect(() => {
-        const request = async () => {
-            let req = await fetch("http://localhost:3000/reports")
-            let res = await req.json()
-            setSightings(res)
-            console.log(res)
-
-        }
-
-        request()
-    }, [])
-
-    console.log(sightings)
+export default function Sighting({ sightings, setSightings, handleSort }) {
     return (
         <div>
             <button className="sortButton" onClick={() => { handleSort() }}>Sort by popularity</button>
             <div className="sighting">
-                {sort ? (
-                    sortedSightings.map((sighting) => {
-                        return (
-                            <SightingCard sighting={sighting} />
-                        )
-                    })
-                ) : (
+                {
                     sightings.map((sighting) => {
                         return (
-                            <SightingCard sighting={sighting} />
+                            <SightingCard key={sighting.id} sighting={sighting} />
                         )
                     })
-                )
                 }
             </div>
         </div>
